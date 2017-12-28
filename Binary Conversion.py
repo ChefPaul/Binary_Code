0adcfb61e6622f646bbb7a731d4488f3e5b9309a
Paul Perez <paul26perez@gmail.com>
2017-12-28 13:26:36 -0500

    Add files via upload

---
+++ Binary Conversion.py
@@ -0,0 +1,29 @@
+def convert_To_Binary(number):
+	binary_string = ""
+
+	while number > 0:
+		binary_string = str(number%2) + binary_string
+
+		number = number // 2
+
+	return binary_string
+
+print( convert_To_Binary(220) )
+
+def convert_From_Binary(binary_string):
+
+	binary_string = binary_string[::-1]
+
+	total = 0
+
+	increment = 1
+
+	for digit in binary_string:
+		if digit == "1":
+			total = total + increment
+
+		increment = increment * 2
+
+	return total
+
+print( convert_From_Binary("10110") )
